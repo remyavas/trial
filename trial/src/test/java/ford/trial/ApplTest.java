89e02ea3373c6c3dbe576e6d050ef5ccc81e64e1
remyavas <remyavas@yahoo.co.in>
2016-09-06 17:44:19 +0530

    added new println statement

--- trial/src/test/java/ford/trial/ApplTest.java
+++ trial/src/test/java/ford/trial/ApplTest.java
@@ -5,6 +5,7 @@ public class ApplTest
 public static void main(String[] args)
 		{
 	System.out.println("hello");
-	System.out.println("hello1");
+	System.out.println("hello2");
+	System.out.println("hello2");
 		}
 }
