43a0bb8a35eb307c75ad4401e14a588336a04a76
remyavas <remyavas@yahoo.co.in>
2016-09-10 15:22:41 +0530

    system.out.println added

--- trial/src/test/java/ford/trial/AppTest.java
+++ trial/src/test/java/ford/trial/AppTest.java
@@ -34,6 +34,7 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
-        System.out.println("hello1011");
+        System.out.println("hello189011");
+        System.out.println("hw r u ");
     }
 }
