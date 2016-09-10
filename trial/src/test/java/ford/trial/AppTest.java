7be73ede6b6dde0795354f8c6ec38de3312fa614
mlmuthu4 <mlmuthu4@gmail.com>
2016-09-10 16:18:50 +0530

    hello seocnd dev  commit

--- trial/src/test/java/ford/trial/AppTest.java
+++ trial/src/test/java/ford/trial/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
-        System.out.println("hello1011");
+       
         System.out.println("alia1");
     }
 }
