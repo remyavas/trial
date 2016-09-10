57953c2cf00b8a0d86d5473144f295066c977a48
remyavas <remyavas@yahoo.co.in>
2016-09-10 15:23:00 +0530

    system.out.println

--- trial/src/test/java/ford/trial/ApplTest.java
+++ trial/src/test/java/ford/trial/ApplTest.java
@@ -6,6 +6,7 @@ public static void main(String[] args)
 		{
 	System.out.println("hello");
 	System.out.println("hello2");
-	System.out.println("hello2");
+	System.out.println("hello2222222");
+	System.out.println("hw r u ");
 		}
 }
